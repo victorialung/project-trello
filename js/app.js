5466e6961a9212f867a8e9ced0ceb4eb615e8242
victorialung <vtr.lung@outlook.es>
2017-11-19 14:06:49 -0500

    modificando version 0.0.2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
-// primero cargamos la pagina con el evento load para "avisar" a los eventos de js que esten disponibles
+// first load the page with load event to "warn" to the events of js to be available
 window.addEventListener('load', function(event) {
-//   primero declaramos nuestras variables mediante getElementById para obtener los elemento HTML
+//   first we declare our variables by getElementById to get the HTML elements
   var container = document.getElementById('container'),
     addListBox = document.getElementById('add-list-box'),
     saveListBox = document.getElementById('save-list-box'),
+    // the form section is here and we can input our text
     inputList = document.getElementById('input-list'),
-    saveList = document.getElementById('save-list');
-
+    saveListBtn = document.getElementById('save-list-btn');
+    
   addListBox.addEventListener('click', function(event) {
     addListBox.style.display = 'none';
     saveListBox.style.display = 'inline-block';
     // console.log(event.target);
+  });
+
+  saveListBtn.addEventListener('click', function(event) {
+    // we keep the information inside an array that is contained within an object, in order to save the different data put in our list
+    saveListBox.style.display = 'inline-block';
+    var dataSaveListBtn = {
+      lists: []
+    };
+
+    var textValue = inputList.value;
+    if (textValue) {
+      var altDiv = document.createElement('div'),
+        altForm = document.createElement('form'),
+        altTitle = document.createElement('div'),
+        altTextNode = document.createTextNode(textValue),
+        altCardTask = document.createElement('a'),
+        altTaskText = document.createTextNode('Add task ...');
+
+        // we call the fir element of dataSaveListBtn's object
+      dataSaveListBtn.lists.push({
+        altTitle: textValue,
+      });
+    }
   });
 });
