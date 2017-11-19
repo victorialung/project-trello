ba52f586bce7d1180b8d71c92d76f902207c0548
victorialung <vtr.lung@outlook.es>
2017-11-19 11:22:08 -0500

    version 0.0.1 terminada

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 // primero cargamos la pagina con el evento load para "avisar" a los eventos de js que esten disponibles
 window.addEventListener('load', function(event) {
-  var addListBoz = document.getElementById('add-list-box');
-  
+//   primero declaramos nuestras variables mediante getElementById para obtener los elemento HTML
+  var container = document.getElementById('container'),
+    addListBox = document.getElementById('add-list-box'),
+    saveListBox = document.getElementById('save-list-box'),
+    inputList = document.getElementById('input-list'),
+    saveList = document.getElementById('save-list');
+
+  addListBox.addEventListener('click', function(event) {
+    addListBox.style.display = 'none';
+    saveListBox.style.display = 'inline-block';
+    // console.log(event.target);
+  });
 });
