f1bcdad923160ab433c6b20835d5e021e6febb50
victorialung <vtr.lung@outlook.es>
2017-11-19 10:19:43 -0500

    html terminado , agregando eventos a js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // primero cargamos la pagina con el evento load para "avisar" a los eventos de js que esten disponibles
-window.addEventListener('load', function(){
-  var 
-
+window.addEventListener('load', function(event) {
+  var addListBoz = document.getElementById('add-list-box');
+  
 });
